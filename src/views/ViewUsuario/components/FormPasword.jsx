b07c3ba02656c29c8fda51c9e799f925e40ceefc
gregoryput <gregory-sanchez7878@hotmail.com>
2023-07-21 23:05:47 -0400

    factorizando componentes de perfil

---
+++ src/views/ViewUsuario/components/FormPasword.jsx
@@ -0,0 +1,49 @@
+import { useForm } from "react-hook-form";
+import { ButtonNext, ContainerForm, InputFor, LabelFor, Row } from "../../../components"
+
+
+export default function FormPasword(){
+    const {
+      formState: { errors },
+  
+    } = useForm();
+    return(
+      <>
+       <ContainerForm>
+         <h3 style={{marginBottom:25,}}>Editar contraseña </h3>
+         
+          <Row>
+          <div style={{marginRight:50}}>
+            <LabelFor>Contraseña  actual</LabelFor>
+           <InputFor type="Password" placeholder="Ingrese los contraseña"/>
+            {errors.Nombres && (
+              <span style={{ color: "red", fontSize: 10 }}>
+                {errors.Nombres.message}
+              </span>
+            )}
+           </div>
+           <div>
+            <LabelFor>Nueva contraseña</LabelFor>
+           <InputFor type="Password" placeholder="Ingrese los contraseña"/>
+            {errors.Nombres && (
+              <span style={{ color: "red", fontSize: 10 }}>
+                {errors.Nombres.message}
+              </span>
+            )}
+           </div>
+           <div style={{marginLeft:50}}>
+            <LabelFor >Confirmar contraseña</LabelFor>
+           <InputFor type="Password" placeholder="Ingrese los contraseña"/>
+            {errors.Nombres && (
+              <span style={{ color: "red", fontSize: 10 }}>
+                {errors.Nombres.message}
+              </span>
+            )}
+           </div>
+            <ButtonNext style={{marginTop:27, marginLeft:50}} >Confirmar</ButtonNext>
+  
+          </Row>
+       </ContainerForm>
+      </>
+    )
+  }
